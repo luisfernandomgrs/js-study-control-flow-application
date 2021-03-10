2d696fc915daa17e6ae7665e399242abe2ed7d9b
Luis Fernando M <luisfernandomgrs@gmail.com>
2021-03-09 22:15:11 -0300

    Code to pratice - using instrctions condictionals, throw, trye and catch

---
+++ js/switch.js
@@ -0,0 +1,53 @@
+// Switch
+
+let expression = 'c';
+
+switch (expression) {
+    case 'a':
+        console.log("a")
+        break;
+    case 'b':
+        console.log("b")
+        break;
+    default:
+        console.log("default...")
+        break;
+}
+
+function Calculate(nParamLeft = 0, nParamRight = 0, sOperator = "+") {
+    let nReturn = 0;
+
+    switch (sOperator) {
+        case '+':
+            nReturn = nParamLeft + nParamRight;
+            break;
+        case '-':
+            nReturn = nParamLeft - nParamRight;
+            break;
+        case '*':
+            nReturn = nParamLeft * nParamRight;
+            break;
+        case '/':
+            nReturn = nParamLeft / nParamRight;
+            break;
+        case '%':
+            nReturn = nParamLeft % nParamRight;
+            break;
+        case '**':
+            nReturn = nParamLeft ** nParamRight;
+            break;
+        default:
+            nReturn = 0;
+            break;
+    }
+
+    console.log("Tipo de Expressão realizada: " + nParamLeft + " " + sOperator + " " + nParamRight);
+    return nReturn;
+}
+
+console.log(Calculate(12,5));
+console.log(Calculate(12,5,"-"));
+console.log(Calculate(12,5,"*"));
+console.log(Calculate(12,6,"/"));
+console.log(Calculate(3,4,"**"));
+console.log(Calculate(12,6,"%"));
---
+++ js/throw-try-catch.js
@@ -0,0 +1,28 @@
+// throw -> Lançar...
+
+function sayMyName(name = "") {
+    if (name === "") {
+        throw new Error("Name is requered!");
+    }
+    
+    // console.log("Code after Throw Error...");
+    console.log("Your name is: " + name);
+}
+
+// Se executar sem um try catch... A aplicação será interrompid.
+// sayMyName();
+
+// try -> Tentar ... catch -> Pegar
+try {
+    sayMyName();
+}
+catch (e) {
+    console.log(e);
+}
+
+try {
+    sayMyName("Luis Fernando");
+}
+catch (e) {
+    console.log(e);
+}
